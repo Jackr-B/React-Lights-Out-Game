71a8a3b8900213f260c0b6efb967742406565dbe
Jackr-B <36901697+Jackr-B@users.noreply.github.com>
2020-06-04 18:06:37 +0100

    Updates

---
+++ precache-manifest.25adb4196509a5e7b8303af9326047db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4922ae753587d1fd4579",
+    "url": "/React-Lights-Out-Game/static/css/main.f787293b.chunk.css"
+  },
+  {
+    "revision": "4922ae753587d1fd4579",
+    "url": "/React-Lights-Out-Game/static/js/main.4922ae75.chunk.js"
+  },
+  {
+    "revision": "498adbd33ac33c4b9d54",
+    "url": "/React-Lights-Out-Game/static/js/1.498adbd3.chunk.js"
+  },
+  {
+    "revision": "cafa4f3494cc60bca7f0",
+    "url": "/React-Lights-Out-Game/static/js/runtime~main.cafa4f34.js"
+  },
+  {
+    "revision": "8a2cef6d233a70ca70cefe19de883f53",
+    "url": "/React-Lights-Out-Game/index.html"
+  }
+];
